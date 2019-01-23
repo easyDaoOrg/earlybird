655ca0c1f5e5fbecc7ee0a95dae681f1da454104
xue.zhao <xue.zhao@tendcloud.com>
2019-01-23 11:27:51 +0800

    修改关于我们图片

--- ui/src/views/brand/about-us.vue
+++ ui/src/views/brand/about-us.vue
@@ -13,7 +13,7 @@
         <p>易道获得业界高度评价，“易道是一个非常新颖的公司。他们真正专注于为旅行者提供有巨大价值的信息和简单流畅的用户体验，因此与易道团队的合作使他们的理念变为现实总是一种愉快的体验，” Sebastien Gibergues说，他是全球Top 1 GDS Amadeus全球休闲及线上旅行项目的负责人。</p>
         <p>易道于 2014年创立，目标是成为全球飞行用户的智能行程优化助手。2017年获得1.3亿元A+轮投资，由沃衍资本、广发证券旗下全资企业广发乾和投资。</p>
       </div>
-      <div class="about-us-content-img"><img src="https://content.igola.com/static/WEB/images/brand/aboutus/brand_aboutus_ZH.jpg" alt="" title=""></div>
+      <div class="about-us-content-img"><img src="../../assets/images/about-us-bg.png" alt="" title="" width="600"></div>
     </div>
   </div>
 </template>
